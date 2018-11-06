7477521530fedc2e4169d70c2f47699cbcf32751
haayman <none@none>
2018-11-06 20:46:39 +0100

    van moment overgestapt naar date-fns

--- startup/config.js
+++ startup/config.js
@@ -3,13 +3,9 @@ const config = require("config");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
-const moment = require('moment');
-
 module.exports = function (app) {
   app.use(helmet());
   if (config.has("useMorgan") && config.get("useMorgan")) {
     app.use(morgan("tiny"));
   }
-
-  moment.locale('nl');
 };
