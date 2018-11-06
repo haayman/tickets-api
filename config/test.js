fa1218bb98f7a496108ccaaa33f305429a14eaf5
haayman <none@none>
2018-11-06 09:14:39 +0100

    vervang .json door .js

---
+++ config/test.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  "jwtPrivateKey": "test",
+  "payment": {
+    "mollie_key": "test_DMVExh3QMmTN6tjUku59cjPzQJNhf8"
+  },
+  database: {
+    storage: path.resolve(__dirname, "..", "data", "test.sqlite"),
+    logging: true
+  },
+  namespace: "PlusLeoTestNamespace", // t.b.v transacties
+
+}
