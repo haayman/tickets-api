ed51ba92fe574c90dab9f9b63cba74816bb64593
haayman <none@none>
2018-11-07 12:07:25 +0100

    - voeg password-strength test toe - kleine verbeteringen login

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,23 @@
 const _ = require("lodash");
 const express = require("express");
-const { User } = require("../models");
+const {
+  User
+} = require("../models");
 const config = require("config");
 const jwt = require("jsonwebtoken");
-
+const checker = require('zxcvbn');
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-  const sendError = function(message = "Onbekende gebruiker of wachtwoord") {
+  const sendError = function (message = "Onbekende gebruiker of wachtwoord") {
     return res.status(400).send(message);
   };
 
   // standaard wordt password niet opgehaald. Doe nu maar wel
   let user = await User.scope("withPassword").findOne({
-    where: { username: req.body.username }
+    where: {
+      username: req.body.username
+    }
   });
   if (!user) return sendError();
 
@@ -21,7 +25,9 @@ router.post("/", async (req, res) => {
   if (!verified) return sendError();
 
   const token = user.getAuthToken();
-  res.header({ authorization: token }).send(user);
+  res.header({
+    authorization: token
+  }).send(user);
 });
 
 router.get("/CheckLoggedIn", async (req, res) => {
@@ -44,5 +50,14 @@ router.get("/CheckLoggedIn", async (req, res) => {
     user: user
   });
 });
+
+router.post("/checkPassword", async (req, res) => {
+  const {
+    password,
+    userInputs
+  } = req.body;
+  const result = checker(password, userInputs);
+  return res.send(result);
+})
 
 module.exports = router;
