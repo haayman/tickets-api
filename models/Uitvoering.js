f37faf3563637630b9e06a7553b50b21906cfd89
haayman <none@none>
2018-11-16 11:55:26 +0100

    verkeerde datum-format

--- models/Uitvoering.js
+++ models/Uitvoering.js
@@ -183,7 +183,7 @@ module.exports = (sequelize, DataTypes) => {
 
   Uitvoering.prototype.toString = function () {
     // https://date-fns.org/v2.0.0-alpha.9/docs/format
-    return `${this.extra_text||''} ${format(this.aanvang, 'dddd d MMM HH:mm', {locale: nl})}`;
+    return `${this.extra_text||''} ${format(this.aanvang, 'dddd D MMM HH:mm', {locale: nl})}`;
 
   }
 
