14832dd87fc12e748716c8182ea883eb6e678329
haayman <none@none>
2018-12-18 14:50:30 +0100

    - locatie incl. link - opmerkingen toegevoegd

--- init_voorstelling.js
+++ init_voorstelling.js
@@ -10,7 +10,8 @@ const {
       title: "Kamers",
       description: "We doen iets met kamers",
       url: "https://plusleo.nl/producties/verframed/",
-      locatie: "nog niet bekend",
+      locatie: "<a href='https://goo.gl/maps/5h7Xsbyrbx62' target='_blank'>Voormalig Kantongerecht<br />Brink 11 / 12<br/>Deventer</a>",
+      opmerkingen: "hou er rekening mee dat het koud kan zijn",
       poster: "https://plusleo.nl/producties/verframed/poster.png",
       thumbnail: "https://plusleo.nl/producties/verframed/thumb.png",
       prijzen: [{
