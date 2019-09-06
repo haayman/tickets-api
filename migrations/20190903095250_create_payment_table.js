53162a36183c7cded609467efbe7bf12cf1cb274
Arjen Haayman <arjen.haayman@gmail.com>
2019-09-06 14:13:02 +0200

    fouten in payments tabel opgelost

--- migrations/20190903095250_create_payment_table.js
+++ migrations/20190903095250_create_payment_table.js
@@ -2,10 +2,10 @@ exports.up = function (knex) {
   return knex.schema
     .createTable('payments', (table) => {
       table.increments('id');
+      table.string('paymentId', 255)
       table.string('betaalstatus', 255);
       table.string('description', 255);
-      table.decimal('paidBack', 10, 5).notNullable();
-      table.string('role', 255);
+      table.decimal('paidBack', 10, 5);
       table.uuid('reserveringId').notNullable();
       table.timestamp('createdAt').defaultTo(knex.fn.now())
       table.timestamp('updatedAt').defaultTo(knex.fn.now())
