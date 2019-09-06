cd4f54fc54cef5dd29fe7dda1e85eece5d9e1a23
Arjen Haayman <arjen.haayman@gmail.com>
2019-09-06 14:10:59 +0200

    laad database voor knex

--- startup/database.js
+++ startup/database.js
@@ -1,38 +1,10 @@
-"use strict";
+const Knex = require('knex');
+const config = require('config');
+const knex = Knex(config.get('database'));
+const {
+  Model
+} = require('objection');
 
-var fs = require("fs");
-var path = require("path");
-var Sequelize = require("sequelize");
-var basename = path.basename(__filename);
-var config = require("config");
-
-// t.b.v. transactions
-const cls = require("continuation-local-storage");
-const namespace = cls.createNamespace(config.get("namespace"));
-Sequelize.useCLS(namespace);
-
-var db = {};
-
-var sequelize = new Sequelize(config.get("database"));
-
-fs.readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-    );
-  })
-  .forEach(file => {
-    var model = sequelize["import"](path.join(__dirname, file));
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
+module.exports = function () {
+  Model.knex(knex);
+}
