7afd670748b537ad619903bcada69a955915f3bb
Arjen Haayman <arjen.haayman@gmail.com>
2019-09-09 19:56:19 +0200

    handmatig alle models inlezen

--- models/index.js
+++ models/index.js
@@ -1,36 +1,7 @@
-"use strict";
-
-const fs = require("fs");
-const path = require("path");
-const Sequelize = require("sequelize");
-const basename = path.basename(__filename);
-const config = require("config");
-let db = {};
-
-const sequelize = new Sequelize(config.get("database"));
-
-fs.readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf(".") !== 0 &&
-      file !== basename &&
-      file.slice(-3) === ".js" &&
-      file.split(".").length == 2 // <Class>.js
-    );
-  })
-  .forEach(file => {
-    const model = sequelize["import"](path.join(__dirname, file));
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-
-module.exports = db;
+module.exports = {
+  User: require('./User'),
+  Voorstelling: require('./Voorstelling'),
+  Uitvoering: require('./Uitvoering'),
+  Prijs: require('./Prijs'),
+  Payment: require('./Payment')
+}
