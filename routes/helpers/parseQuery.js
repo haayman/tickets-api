64b396db7c346714ad9627b9ed8a6cba47207ff3
Arjen Haayman <arjen.haayman@gmail.com>
2019-09-09 19:57:25 +0200

    gebruik eager syntax

--- routes/helpers/parseQuery.js
+++ routes/helpers/parseQuery.js
@@ -1,45 +1,29 @@
-function getIncludes(model, params, includes = []) {
-  params.forEach(param => {
-    const levels = param.split(".");
-    const association = model[levels[0]];
-    if (association) {
-      let newInclude = {
-        association: association
-      };
-      if (levels.length > 1) {
-        const nextLevel = levels[1];
-        const subIncludes = nextLevel.split(',').map((subInclude) => [subInclude].concat(levels.slice(2)).join('.'))
-        newInclude.include = getIncludes(association.target, subIncludes);
-      }
-      includes.push(newInclude);
-    } else {
-      throw new Error(`${model.tableName} has no association ${levels[0]}`);
-    }
-  });
-  return includes;
-}
+/**
+ * 
+ * @param {QueryBuilder} query 
+ * @param {{include:[table,...],order:['column']}} params 
+ */
+function parseQuery(query, params) {
+  if (params.include) {
+    const eager = JSON.stringify(params.include).replace(/\"/g, '');
 
-function parseQuery(model, query) {
-  let params = query;
-  if (query.include) {
-    params.include = getIncludes(model, query.include);
+    query.eager(eager);
   }
+
   if (params.order) {
-    let order = []
     if (!Array.isArray(params.order)) {
       params.order = [params.order];
     }
     params.order.forEach(p => {
       const matches = p.match(/^-(.+$)/);
       if (matches) {
-        order.push([matches[1], 'desc'])
+        query.orderBy(matches[1], 'desc')
       } else {
-        order.push([p, 'asc']);
+        query.orderBy(p);
       }
     })
-    params.order = order;
   }
-  return params;
+  return query
 }
 
 module.exports = parseQuery;
