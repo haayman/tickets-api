f7a47c6b0baa0ed6e8340f1fcb993cdb0a9439e2
Arjen Haayman <arjen.haayman@gmail.com>
2021-03-14 20:59:41 +0100

    paginering gebruikt geen offset maar page

--- routes/helpers/parseQuery.js
+++ routes/helpers/parseQuery.js
@@ -5,7 +5,7 @@
  */
 function parseQuery(query, params) {
   if (params.include) {
-    const withGraphFetched = JSON.stringify(params.include).replace(/\"/g, '');
+    const withGraphFetched = JSON.stringify(params.include).replace(/\"/g, "");
 
     query.withGraphJoined(withGraphFetched);
   }
@@ -17,14 +17,14 @@ function parseQuery(query, params) {
     params.order.forEach((p) => {
       const matches = p.match(/^-(.+$)/);
       if (matches) {
-        query.orderBy(matches[1], 'desc');
+        query.orderBy(matches[1], "desc");
       } else {
         query.orderBy(p);
       }
     });
   }
   if (params.limit) {
-    query.page(+params.offset || 0, +params.limit);
+    query.page(+params.page || 0, +params.limit);
   }
   return query;
 }
