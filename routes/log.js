e88e7a4b2da02af87e6dfac6d1b6ce2ba601be79
Arjen Haayman <arjen.haayman@gmail.com>
2021-03-14 20:59:41 +0100

    paginering gebruikt geen offset maar page

--- routes/log.js
+++ routes/log.js
@@ -1,17 +1,15 @@
 const express = require("express");
-const auth = require('../middleware/auth');
-const {
-  Log
-} = require("../models");
-const parseQuery = require('./helpers/parseQuery');
+const auth = require("../middleware/auth");
+const { Log } = require("../models");
+const parseQuery = require("./helpers/parseQuery");
 
 const router = express.Router();
 
 router.get("/", auth(true), async (req, res) => {
   let params = req.query;
 
-  const query = parseQuery(Log.query().allowGraph('[reservering]'), params)
-  const logs = await query.orderBy('id', 'desc');
+  const query = parseQuery(Log.query().allowGraph("[reservering]"), params);
+  const logs = await query.orderBy("id", "desc");
   res.send(logs.results);
 });
 
