4fb2ab963b9ea8daf4cb44c8b1ec4c4d09f4b413
Arjen Haayman <arjen.haayman@gmail.com>
2021-12-12 13:54:05 +0100

    aparte handler voor reserveringDeleted

---
+++ handlers/reserveringDeleted.ts
@@ -0,0 +1,34 @@
+import { EntityManager } from "@mikro-orm/core";
+import Container from "typedi";
+import { Reservering } from "../models";
+import { RefundHandler } from "../helpers/RefundHandler";
+import { ReserveringMail } from "../components/ReserveringMail";
+import winston from "winston";
+
+export async function reserveringDeleted(reserveringId: string) {
+  winston.info(`reserveringDeleted ${reserveringId}`);
+  setTimeout(() => {
+    RefundHandler.verwerkRefunds();
+  }, 1000);
+
+  setTimeout(async () => {
+    const em: EntityManager = (Container.get("em") as EntityManager).fork();
+    await em.transactional(async (em) => {
+      const repository = em.getRepository<Reservering>("Reservering");
+      const reservering = await repository.findOne(
+        { id: reserveringId },
+        Reservering.populate()
+      );
+
+      if (!reservering.tickets.getItems().length) {
+        repository.remove(reservering);
+      } else {
+        await ReserveringMail.send(
+          reservering,
+          "gewijzigd",
+          `${reservering} te koop aangeboden`
+        );
+      }
+    });
+  }, 500);
+}
