63a731fb21d37a6aebdf1b1caaf890e297376f79
Arjen Haayman <arjen.haayman@gmail.com>
2021-12-21 20:38:44 +0100

    verkoop werkt

--- handlers/verwerkTekoop.ts
+++ handlers/verwerkTekoop.ts
@@ -1,30 +1,44 @@
 import { EntityManager } from "@mikro-orm/core";
 import { Container } from "typedi";
-import { Log, Ticket } from "../models";
+import winston from "winston";
+import { ReserveringMail } from "../components/ReserveringMail";
+import { Log, Ticket, Reservering } from "../models";
 
 export async function verwerkTekoop(aantal: number) {
+  if (!aantal) return;
+  winston.info(`verwerkTekoop(${aantal})`);
   const em: EntityManager = Container.get("em");
-  await em.transactional(async (em) => {
-    const repository = em.getRepository<Ticket>("Ticket");
-    const tekoop = await repository.find(
-      { tekoop: true },
-      {
-        orderBy: { created_at: "asc" },
-        limit: aantal,
-        populate: ["reservering.logs"],
-      }
-    );
-
-    for (const ticket of tekoop) {
-      const reservering = ticket.reservering;
-
-      await ticket.finishLoading();
-
-      ticket.verkocht = true;
-      ticket.tekoop = false;
-      ticket.terugbetalen = true;
-
-      Log.addMessage(reservering, `${ticket} verkocht`);
+  const repository = em.getRepository<Ticket>("Ticket");
+  const tekoop = await repository.find(
+    { tekoop: true },
+    {
+      orderBy: { created_at: "asc" },
+      limit: aantal,
+      populate: ["prijs", "reservering.logs", "reservering.uitvoering"],
     }
-  });
+  );
+
+  const reserveringen: Map<Reservering, Ticket[]> = new Map();
+  for (const ticket of tekoop) {
+    const reservering = ticket.reservering;
+    if (!reserveringen.has(reservering)) {
+      reserveringen.set(reservering, []);
+    }
+    const tickets = reserveringen.get(reservering);
+
+    await ticket.finishLoading();
+
+    ticket.verkocht = true;
+    ticket.tekoop = false;
+    ticket.terugbetalen = true;
+
+    reserveringen.set(reservering, [...tickets, ticket]);
+  }
+
+  for (let [reservering, tickets] of reserveringen) {
+    await em.populate(reservering, Reservering.populate());
+    const description = Ticket.description(tickets);
+    Log.addMessage(reservering, `${description} verkocht`);
+    ReserveringMail.send(reservering, "verkocht", `${description} verkocht`);
+  }
 }
