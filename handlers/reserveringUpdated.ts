17dd0596e2937fdf4f038c1c5315550d874533ba
Arjen Haayman <arjen.haayman@gmail.com>
2022-01-23 21:08:17 +0100

    wacht met mails zodat wijzigingen doorgevoerd zijn (werkt niet zo goed)

--- handlers/reserveringUpdated.ts
+++ handlers/reserveringUpdated.ts
@@ -14,45 +14,47 @@ export async function reserveringUpdated(
   winston.info(`reserveringUpdated, ${reserveringId}`);
   RefundHandler.verwerkRefunds();
 
-  const em: EntityManager = (Container.get("em") as EntityManager).fork();
-  await em.begin();
-  try {
-    const repository = em.getRepository<Reservering>("Reservering");
-    const reservering = await repository.findOne(
-      { id: reserveringId },
-      Reservering.populate()
-    );
-    if (!reservering) {
-      winston.error(`reservering ${reserveringId} niet gevonden`);
-      return;
+  setTimeout(async () => {
+    const em: EntityManager = (Container.get("em") as EntityManager).fork();
+    await em.begin();
+    try {
+      const repository = em.getRepository<Reservering>("Reservering");
+      const reservering = await repository.findOne(
+        { id: reserveringId },
+        Reservering.populate()
+      );
+      if (!reservering) {
+        winston.error(`reservering ${reserveringId} niet gevonden`);
+        return;
+      }
+      await reservering.finishLoading();
+      const saldo = reservering.saldo;
+      if (saldo >= 0) {
+        await ReserveringMail.send(
+          reservering,
+          "ticket",
+          `kaarten voor ${reservering}`
+        );
+      } else if (reservering.wachtlijst) {
+        await ReserveringMail.send(
+          reservering,
+          "wachtlijst",
+          "Je staat op de wachtlijst"
+        );
+      } else {
+        await ReserveringMail.send(
+          reservering,
+          "gewijzigd",
+          `${reservering} gewijzigd`
+        );
+      }
+      await em.commit();
+    } catch (e) {
+      winston.error(e);
+      await em.rollback();
+    } finally {
+      const queue = getQueue();
+      queue.emit("reserveringUpdatedDone", "");
     }
-    await reservering.finishLoading();
-    const saldo = reservering.saldo;
-    if (saldo >= 0) {
-      await ReserveringMail.send(
-        reservering,
-        "ticket",
-        `kaarten voor ${reservering}`
-      );
-    } else if (reservering.wachtlijst) {
-      await ReserveringMail.send(
-        reservering,
-        "wachtlijst",
-        "Je staat op de wachtlijst"
-      );
-    } else {
-      await ReserveringMail.send(
-        reservering,
-        "gewijzigd",
-        `${reservering} gewijzigd`
-      );
-    }
-    await em.commit();
-  } catch (e) {
-    winston.error(e);
-    await em.rollback();
-  } finally {
-    const queue = getQueue();
-    queue.emit("reserveringUpdatedDone", "");
-  }
+  }, 100);
 }
