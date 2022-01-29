e46ea0ab32f2cb706bea6bf2c266ba8e7e2e92a2
Arjen Haayman <arjen.haayman@gmail.com>
2022-01-29 10:16:16 +0100

    nodemailer mock werkend

--- jest.config.js
+++ jest.config.js
@@ -108,7 +108,7 @@ module.exports = {
   // runner: "jest-runner",
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
-  // setupFiles: [],
+  setupFiles: ["<rootDir>/__mocks__/nodemailer.js", "<rootDir>/tests/setup.js"],
 
   // The path to a module that runs some code to configure or set up the testing framework before each test
   // setupTestFrameworkScriptFile: './tests/setup.js',
