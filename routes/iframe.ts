7124f9ea505df204d88bce3cd9c26c5beb34baac
Arjen Haayman <arjen.haayman@gmail.com>
2024-02-02 14:15:58 +0100

    ondersteun meerdere voorstellingen in iframe

--- routes/iframe.ts
+++ routes/iframe.ts
@@ -15,20 +15,57 @@ router.get("/", async (req, res) => {
   const voorstellingen = await repository.findAll({
     populate: ["uitvoeringen", "prijzen"],
   });
-  const voorstelling = voorstellingen[0];
 
-  aejs.renderFile(
-    __dirname + "/templates/iframe.ejs",
-    {
-      voorstelling,
-      format,
-      nl,
-      env: process.env,
-    },
-    (error, result) => {
-      res.send(result);
+  try {
+    const voorstellingId = req.query.id;
+    if (voorstellingId) {
+      const voorstelling = voorstellingen.find((v) => v.id === +voorstellingId);
+      if (voorstelling) {
+        return aejs.renderFile(
+          __dirname + "/templates/iframe.ejs",
+          {
+            voorstelling,
+            format,
+            nl,
+            env: process.env,
+          },
+          (error, result) => {
+            res.send(result);
+          }
+        );
+      }
+    } else if (voorstellingen.length === 1) {
+      const voorstelling = voorstellingen[0];
+
+      aejs.renderFile(
+        __dirname + "/templates/iframe.ejs",
+        {
+          voorstelling,
+          format,
+          nl,
+          env: process.env,
+        },
+        (error, result) => {
+          res.send(result);
+        }
+      );
+    } else {
+      aejs.renderFile(
+        __dirname + "/templates/iframe-list.ejs",
+        {
+          voorstellingen,
+          format,
+          nl,
+          env: process.env,
+        },
+        (error, result) => {
+          res.send(result);
+        }
+      );
     }
-  );
+  } catch (e) {
+    return "Er gaat iets fout";
+  }
 });
 
 export default router;
